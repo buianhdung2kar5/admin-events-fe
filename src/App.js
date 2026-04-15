13c79c5c0a2f2e43096e944bd5c1d4c674b69a95
Dungne <bed209fit@gmail.com>
2026-04-15 14:21:14 +0700

    setup ci/cd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainRoutes from "./config/layouts/MainRoutes";
 import Navbar from "./config/layouts/Navbar";
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename="/admin-events-fe" >
    <div className="flex flex-col lg:flex-row min-h-screen bg-gray-50">
     <Navbar/>
     <div className="flex-1 w-full min-w-0 overflow-hidden p-4 lg:p-8">
